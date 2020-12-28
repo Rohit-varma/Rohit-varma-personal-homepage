550310b0e9fd757f8839efca7b3d98965848fc94
Rohit-varma <44708807+Rohit-varma@users.noreply.github.com>
2020-12-28 10:56:50 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,11 +4,11 @@ import PropTypes from 'prop-types'
 const Footer = (props) => (
     <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
         
-        <p className="copyright">
-         Built with <a href="http://gatsby-dimension.surge.sh">Gatsby.js</a> and ❤️ by <a href="https://rohit-varma.netlify.app/"> Rohit Varma</a>  
-        <br/>
-        Get your own version of this webpage <a href="https://github.com/Rohit-varma/Rohit-varma-personal-homepage"> here </a>
-        </p>
+//         <p className="copyright">
+//          Built with <a href="http://gatsby-dimension.surge.sh">Gatsby.js</a> and ❤️ by <a href="https://rohit-varma.netlify.app/"> Rohit Varma</a>  
+//         <br/>
+//         Get your own version of this webpage <a href="https://github.com/Rohit-varma/Rohit-varma-personal-homepage"> here </a>
+//         </p>
     </footer>
 )
 
